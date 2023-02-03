3cd76974b01eb19e14efaab0a51dfb5714a18738
marteom <marteom@gmail.com>
2023-02-03 18:57:21 +0300

    feat: controller skeleton for user

---
+++ src/user/user.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UserModel } from './user.model';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+
+@Controller('user')
+export class UserController {
+
+    @Get() 
+    async getAllUsers()
+    {
+        return [];
+    }
+
+    @Get(':id') 
+    async getUserById(@Param('id') id: string)
+    {
+        return {};
+    }
+
+    @Post()
+    async CreateUserDto(@Body() dto: Pick<UserModel, 'login' | 'password'>) {
+        return {};
+    }
+
+    @Put(':id')
+    async updateUserPasword(@Param('id') id: string, @Body() dto: UpdatePasswordDto) {
+        return {};
+    }
+
+    @Delete(':id') 
+    async deleteUserById(@Param('id') id: string)
+    {
+        return {};
+    }
+}
